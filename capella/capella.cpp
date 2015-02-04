c2faafce09b32982b28b6c1712fb55670ff40cac
Hirayama Naoyuki <hirayama@creatures.co.jp>
2015-02-04 11:44:51 +0900

    change coding style to google's one

--- capella/capella.cpp
+++ capella/capella.cpp
@@ -22,14 +22,13 @@ using namespace capella;
 
 ////////////////////////////////////////////////////////////////
 // scanner
-template < class It >
+template <class It>
 class scanner {
 public:
     typedef int char_type;
 
 public:
-    scanner( It b, It e ) : b_(b), e_(e), c_(b), unget_count_(0)
-    {
+    scanner(It b, It e) : b_(b), e_(e), c_(b), unget_count_(0) {
         addr_ = 0;
         reserved_["type"] = token_TypeDef;
         reserved_["base"] = token_BaseDef;
@@ -38,57 +37,54 @@ public:
         reserved_["class-footer"] = token_ClassFooterDef;
         reserved_["module-header"] = token_ModuleHeaderDef;
         reserved_["module-footer"] = token_ModuleFooterDef;
-        lines_.push_back( 0 );
+        lines_.push_back(0);
     }
     ~scanner(){}
 
     int addr() { return addr_; }
 
-    int lineno( int addr )
-    {
-        std::vector<int>::const_iterator i = std::upper_bound( lines_.begin(), lines_.end(), addr );
-        assert( i != lines_.begin() );
-        return int( i - lines_.begin() );
+    int lineno(int addr) {
+        std::vector<int>::const_iterator i = std::upper_bound(lines_.begin(), lines_.end(), addr);
+        assert(i != lines_.begin());
+        return int(i - lines_.begin());
     }
-    int column( int addr )
-    {
-        std::vector<int>::const_iterator i = std::upper_bound( lines_.begin(), lines_.end(), addr );
-        assert( i != lines_.begin() );
+    int column(int addr) {
+        std::vector<int>::const_iterator i = std::upper_bound(lines_.begin(), lines_.end(), addr);
+        assert(i != lines_.begin());
         --i;
         return addr - *i;
     }
 
-    Token get( Value& v )
-    {
+    Token get(Value& v) {
         int c;
         do {
             c = sgetc();
-        } while( isspace( c ) );
+        } while (isspace(c));
 
         int b = addr_ - 1;
 
-        // ‹L†—Ş
-        switch( c ) {
-        case '(': v = Value( range( b, addr_ ), Operator( '(' ) ); return token_LParen;
-        case ')': v = Value( range( b, addr_ ), Operator( ')' ) ); return token_RParen;
-        case '|': v = Value( range( b, addr_ ), Operator( '|' ) ); return token_Pipe;
-        case '*': v = Value( range( b, addr_ ), Operator( '=' ) ); return token_List;
-        case '=': v = Value( range( b, addr_ ), Operator( '=' ) ); return token_Equal;
-        case ',': v = Value( range( b, addr_ ), Operator( ':' ) ); return token_Comma;
-        case ';': v = Value( range( b, addr_ ), Operator( ';' ) ); return token_Semicolon;
-        case EOF: v = Value( range( b, addr_ ), Operator( '$' ) ); return token_eof;
+        // è¨˜å·é¡
+        switch (c) {
+            case '(': v = Value(range(b, addr_), Operator('(')); return token_LParen;
+            case ')': v = Value(range(b, addr_), Operator(')')); return token_RParen;
+            case '|': v = Value(range(b, addr_), Operator('|')); return token_Pipe;
+            case '*': v = Value(range(b, addr_), Operator('=')); return token_List;
+            case '=': v = Value(range(b, addr_), Operator('=')); return token_Equal;
+            case ',': v = Value(range(b, addr_), Operator(':')); return token_Comma;
+            case ';': v = Value(range(b, addr_), Operator(';')); return token_Semicolon;
+            case EOF: v = Value(range(b, addr_), Operator('$')); return token_eof;
         }
 
-        // ¯•Êq
-        if( isalpha( c ) || c == '_' || c == ':' ) {
+        // è­˜åˆ¥å­
+        if (isalpha(c)|| c == '_' || c == ':') {
             std::stringstream ss;
-            while( c != EOF && ( isalpha( c ) || isdigit( c ) || c=='-' || c == '_' || c == ':' ) ) {
-                if( c == ':' ) {
+            while (c != EOF &&(isalpha(c)|| isdigit(c)|| c == '-' || c == '_' || c == ':')) {
+                if (c == ':') {
                     c = sgetc();
-                    if( c == ':' ) {
+                    if (c == ':') {
                         ss << "::";
                     } else {
-                        sungetc( c );
+                        sungetc(c);
                         break;
                     }
                 } else {
@@ -96,50 +92,50 @@ public:
                 }
                 c = sgetc();
             }
-            sungetc( c );
+            sungetc(c);
 
-            reserved_type::const_iterator  i = reserved_.find( ss.str() );
-            if( i != reserved_.end() ) {
-                v = Value( range( b, addr_ ), Reserved( ss.str() ) );
-                return (*i).second;
+            reserved_type::const_iterator  i = reserved_.find(ss.str());
+            if (i != reserved_.end()) {
+                v = Value(range(b, addr_), Reserved(ss.str()));
+                return(*i).second;
             } else {
-                v = Value( range( b, addr_ ), Identifier( ss.str() ) );
+                v = Value(range(b, addr_), Identifier(ss.str()));
                 return token_Identifier;
             }
         }
 
-        // –„‚ß‚İ
-        if( c == '@' ) {
+        // åŸ‹ã‚è¾¼ã¿
+        if (c == '@') {
             c = sgetc();
-            if( c != '{' ) {
-                throw unexpected_char( addr_, c );
+            if (c != '{') {
+                throw unexpected_char(addr_, c);
             }
 
             bool first_cr = true;
 
             std::stringstream ss;
-            for( ;; ) {
+            for (;;) {
                 c = sgetc();
-                if( c == '}' ) {
+                if (c == '}') {
                     c = sgetc();
-                    if( c == '@' ) {
+                    if (c == '@') {
                         break;
                     }
-                    sungetc( c );
+                    sungetc(c);
                     ss << '}';
-                } else if( c == EOF ) {
-                    throw mismatch_paren( addr_, c );
-                } else if( c == '\n' ) {
-                    if( first_cr ) {
+                } else if (c == EOF) {
+                    throw mismatch_paren(addr_, c);
+                } else if (c == '\n') {
+                    if (first_cr) {
                         std::string s = ss.str();
                         size_t i;
-                        for( i = 0 ; i < s.length() ; i++ ) {
-                            if( !isspace( s[i] ) )  {
+                        for (i = 0 ; i < s.length(); i++) {
+                            if (!isspace(s[i])) {
                                 break;
                             }
                         }
-                        if( i == s.length() ) {
-                            ss.str( "" );
+                        if (i == s.length()) {
+                            ss.str("");
                         } else {
                             ss << (char)c;
                         }
@@ -151,57 +147,57 @@ public:
                     ss << (char)c;
                 }
             }
-            v = Value( range( b, addr_ ), BulkText( ss.str() ) );
+            v = Value(range(b, addr_), BulkText(ss.str()));
             return token_BulkText;
         }
 
-        throw unexpected_char( addr_, c );
+        throw unexpected_char(addr_, c);
     }
 
 private:
-    char_type sgetc()
-    {
+    char_type sgetc() {
         int c;
-        if( 0 < unget_count_ ) {
+        if (0 < unget_count_) {
             c = unget_[--unget_count_];
-        } else if( c_ == e_ ) {
+        } else if (c_ == e_) {
             c = EOF; 
         } else {
             c = *c_++;
-            if( c == '\n' ) {
-                lines_.push_back( addr_+1 );
+            if (c == '\n') {
+                lines_.push_back(addr_+1);
             }
         }
         addr_++;
         return c;
     }
 
-    void sungetc( char_type c )
-    {
-        if( c != EOF ) {
+    void sungetc(char_type c) {
+        if (c != EOF) {
             addr_--;
             unget_[unget_count_++] = c;
         }
     }
 
-    void push_paren( std::vector<char>& stack, int c )
-    {
-        assert( c == '{' );
-        stack.push_back( c );
+    void push_paren(std::vector<char>& stack, int c) {
+        assert(c == '{');
+        stack.push_back(c);
     }
 
-    void pop_paren( std::vector<char>& stack, int c )
-    {
-        assert( !stack.empty() );
-        switch( c ) {
-        case '}': if( stack.back() != '{' ) { throw mismatch_paren( addr_, c ); } break;
-        default: assert(0);
+    void pop_paren(std::vector<char>& stack, int c) {
+        assert(!stack.empty());
+        switch (c) {
+            case '}':
+                if (stack.back() != '{') {
+                    throw mismatch_paren(addr_, c);
+                }
+                break;
+            default: assert(0);
         }
         stack.pop_back();
     }
 
 private:
-    Range range( int b, int e ) { return Range( b, e ); }
+    Range range(int b, int e) { return Range(b, e); }
 
 private:
     It              b_;
@@ -211,7 +207,7 @@ private:
     char_type       unget_[2];
     int             addr_;
 
-    typedef std::map< std::string, Token > reserved_type;
+    typedef std::map<std::string, Token> reserved_type;
     reserved_type reserved_;
 
     std::vector<int> lines_;
@@ -223,11 +219,11 @@ struct SemanticAction {
     void syntax_error(){}
     void stack_overflow(){}
 
-    template < class T >
-    void downcast( T& x, Value y ) { x = boost::get<T>( y.data ); }
+    template <class T>
+    void downcast(T& x, Value y) { x = boost::get<T>(y.data); }
 
-    template < class T >
-    void upcast( Value& x, const T& y ) { x.data = Node( y ); }
+    template <class T>
+    void upcast(Value& x, const T& y) { x.data = Node(y); }
 
     template <class T>
     Module MakeModule(const T& x) {
@@ -310,12 +306,13 @@ struct SemanticAction {
 ////////////////////////////////////////////////////////////////
 // IdentifierCollector
 struct IdentifierCollector : public boost::static_visitor<void> {
-    IdentifierCollector( typeset_type& t, atomset_type& s ) : types( t ), atoms( s ) {}
+    IdentifierCollector(typeset_type& t, atomset_type& s)
+        : types(t), atoms(s) {}
     typeset_type& types;
     atomset_type& atoms;
 
-    template < class T >
-    void operator()( const T& ) const {}
+    template <class T>
+    void operator()(const T&) const {}
 
     template <class T>
     void apply_to_vector(const std::vector<T>& x) const {
@@ -348,43 +345,43 @@ struct commandline_options {
 void get_commandline_options(
     commandline_options&    cmdopt,
     int                     argc,
-    char**                  argv )
-{
+    char**                  argv) {
     cmdopt.language = "C++";
 
     int state = 0;
-    for( int index = 1 ; index < argc ; index++ ) {
-        if( argv[index][0] == '-' ) {
-            if( strcmp( argv[index], "-c++" ) == 0 ||
-                strcmp( argv[index], "-cpp" ) == 0 ) {
+    for (int index = 1 ; index <argc ; index++) {
+        if (argv[index][0] == '-') {
+            if (strcmp(argv[index], "-c++") == 0 ||
+                strcmp(argv[index], "-cpp") == 0) {
                 cmdopt.language = "C++";
                 continue;
             }
-            if( strcmp( argv[index], "-c++-shared" ) == 0 ||
-                strcmp( argv[index], "-cpp-shared" ) == 0 ) {
+            if (strcmp(argv[index], "-c++-shared") == 0 ||
+                strcmp(argv[index], "-cpp-shared") == 0) {
                 cmdopt.language = "C++-shared";
                 continue;
             }
-            if( strcmp( argv[index], "-c++-variant" ) == 0 ||
-                strcmp( argv[index], "-cpp-variant" ) == 0 ) {
+            if (strcmp(argv[index], "-c++-variant") == 0 ||
+                strcmp(argv[index], "-cpp-variant") == 0) {
                 cmdopt.language = "C++-variant";
                 continue;
             }
-            if( strcmp( argv[index], "-c++-stub" ) == 0 || strcmp( argv[index], "-cpp-stub" ) == 0 ) {
+            if (strcmp(argv[index], "-c++-stub") == 0 ||
+                strcmp(argv[index], "-cpp-stub") == 0) {
                 cmdopt.language = "C++-stub";
                 continue;
             }
-            if( strcmp( argv[index], "-c++-shared-stub" ) == 0 ||
-                strcmp( argv[index], "-cpp-shared-stub" ) == 0 ) {
+            if (strcmp(argv[index], "-c++-shared-stub") == 0 ||
+                strcmp(argv[index], "-cpp-shared-stub") == 0) {
                 cmdopt.language = "C++-shared-stub";
                 continue;
             }
-            if( strcmp( argv[index], "-c++-variant-stub" ) == 0 ||
-                strcmp( argv[index], "-cpp-variant-stub" ) == 0 ) {
+            if (strcmp(argv[index], "-c++-variant-stub") == 0 ||
+                strcmp(argv[index], "-cpp-variant-stub") == 0) {
                 cmdopt.language = "c++-variant-stub";
                 continue;
             }
-            if( strcmp( argv[index], "-dot" ) == 0 ) {
+            if (strcmp(argv[index], "-dot") == 0) {
                 cmdopt.language = "dot";
                 continue;
             }
@@ -392,100 +389,99 @@ void get_commandline_options(
             exit(1);
         }
 
-        switch( state ) {
-        case 0: cmdopt.infile  = argv[index]; state++; break;
-        case 1: cmdopt.outfile = argv[index]; state++; break;
-        default:
-            std::cerr << "too many arguments" << std::endl;
-            exit(1);
+        switch (state) {
+            case 0: cmdopt.infile = argv[index]; state++; break;
+            case 1: cmdopt.outfile = argv[index]; state++; break;
+            default:
+                std::cerr << "too many arguments" << std::endl;
+                exit(1);
         }
     }
 
-    if( state < 2 ) {
-        std::cerr << "usage: capella [ -c++ | -c++-shared | -boost | -dot ] input_filename output_filename\n";
+    if (state < 2) {
+        std::cerr << "usage: capella [-c++ | -c++-shared | -boost | -dot] input_filename output_filename\n";
         exit(1);
     }
-        
+
 }
 
 ////////////////////////////////////////////////////////////////
 // main
-int main( int argc, char** argv )
-{
+int main(int argc, char** argv) {
     commandline_options cmdopt;
-    get_commandline_options( cmdopt, argc, argv );
+    get_commandline_options(cmdopt, argc, argv);
 
-    typedef  void ( *generator_type )(
+    typedef  void(*generator_type)(
         const std::string&,
         std::ostream&,
         Dependency&,
         const typeset_type& types,
         const atomset_type& atoms,
-        const Value& );
+        const Value&);
 
-    std::map< std::string, generator_type > generators;
-    generators["C++"]               = generate_cpp_normal;
-    generators["C++-shared"]        = generate_cpp_shared;
-    generators["C++-variant"]       = generate_cpp_variant;
-    generators["C++-stub"]          = generate_stub_cpp_normal;
-    generators["C++-shared-stub"]   = generate_stub_cpp_shared;
-    generators["C++-variant-stub"]  = generate_stub_cpp_variant;
-    generators["dot"]               = generate_dot;
+    std::map<std::string, generator_type> generators;
+    generators["C++"] = generate_cpp_normal;
+    generators["C++-shared"] = generate_cpp_shared;
+    generators["C++-variant"] = generate_cpp_variant;
+    generators["C++-stub"] = generate_stub_cpp_normal;
+    generators["C++-shared-stub"] = generate_stub_cpp_shared;
+    generators["C++-variant-stub"] = generate_stub_cpp_variant;
+    generators["dot"] = generate_dot;
 
-    std::ifstream ifs( cmdopt.infile.c_str() );
-    if( !ifs ) {
+    std::ifstream ifs(cmdopt.infile.c_str());
+    if (!ifs) {
         std::cerr << "can't open input file '" << cmdopt.infile << "'" << std::endl;
         exit(1);
     }
 
-    std::ofstream ofs( cmdopt.outfile.c_str() );
-    if( !ofs ) {
+    std::ofstream ofs(cmdopt.outfile.c_str());
+    if (!ofs) {
         std::cerr << "can't open output file '" << cmdopt.outfile << "'" << std::endl;
         exit(1);
     }
 
-    if( generators.find( cmdopt.language ) == generators.end() ) {
+    if (generators.find(cmdopt.language) == generators.end()) {
         std::cerr << "no such language supported: " << cmdopt.language << std::endl;
         exit(1);
     }
 
     try {
-        // ƒXƒLƒƒƒi
+        // ã‚¹ã‚­ãƒ£ãƒŠ
         typedef std::istreambuf_iterator<char> is_iterator;
-        is_iterator b( ifs );   // ‘¦’l‚É‚·‚é‚ÆVC++‚ª“Ú’¿Š¿‚È‚±‚Æ‚ğŒ¾‚¤
+        is_iterator b(ifs);
         is_iterator e;
-        scanner< is_iterator > s( b, e );
+        scanner<is_iterator> s(b, e);
 
         SemanticAction sa;
-        Parser< Value, SemanticAction > parser( sa );
+        Parser<Value, SemanticAction> parser(sa);
 
         Token token;
-        for(;;) {
+        for (;;) {
             Value v;
-            token = s.get( v );
-            if( parser.post( token, v ) ) { break; }
+            token = s.get(v);
+            if (parser.post(token, v)) { break; }
         }
 
-        if( parser.error() ) {
-            std::cerr << "syntax error at line: " << s.lineno( s.addr() ) << std::endl;
+        if (parser.error()) {
+            std::cerr << "syntax error at line: " << s.lineno(s.addr()) << std::endl;
             exit(1);
         }
 
         Value v;
-        if( parser.accept( v ) ) {
+        if (parser.accept(v)) {
             Dependency dependency;
-            make_dependency( dependency, v );
+            make_dependency(dependency, v);
 
             typeset_type types;
             atomset_type atoms;
-            boost::apply_visitor( IdentifierCollector( types, atoms ), v.data );
+            boost::apply_visitor(IdentifierCollector(types, atoms), v.data);
 
-            generators[cmdopt.language]( argv[2], ofs, dependency, types, atoms, v );
+            generators[cmdopt.language](argv[2], ofs, dependency, types, atoms, v);
 #if 0
 
             std::cerr << "atoms: ";
-            for( atomset_type::const_iterator i = atoms.begin() ; i != atoms.end() ; ++i ) {
-                std::cerr << *i << ", ";
+            for (const auto& e: atoms) {
+                std::cerr << e << ", ";
             }
             std::cerr << std::endl;
 
@@ -494,7 +490,7 @@ int main( int argc, char** argv )
 
         }
     }
-    catch( capella_error& e ) {
+    catch(capella_error& e) {
         std::cerr << e.what() << std::endl;
         exit(1);
     }
